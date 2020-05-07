ef1a8afb3ed2979c46b35ee7595bc9b0f4c5e379
MuteBard <csevere@brandeis.edu>
2020-05-07 13:20:29 -0400

    Added route usage in header, wiring active tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/styles'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Button from '@material-ui/core/Button'
+import { Link } from "react-router-dom"
 
 import logo from '../../assets/logo.svg'
 
@@ -29,7 +30,13 @@ const useStyles = makeStyles(theme => ({
     marginBottom: "3em"
   },
   logo: {
-    height : "7em"
+    height : "8em"
+  },
+  logoContainer:{
+    padding : 0,
+    "&:hover":{
+      backgroundColor : "transparent"
+    }
   },
   tabsContainer: {
     marginLeft: "auto"
@@ -55,6 +62,20 @@ function Header(props){
   const handleChange = (e, value) => {
     setValue(value)
   }
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0){
+      setValue(0)
+    } else if (window.location.pathname === "/services" && value !== 1){
+      setValue(1)
+    } else if (window.location.pathname === "/revolution" && value !== 2){
+      setValue(2)
+    } else if (window.location.pathname === "/about" && value !== 3){
+      setValue(3)
+    } else if (window.location.pathname === "/contact" && value !== 4){
+      setValue(4)
+    }
+
+  })
 
   //https://material-ui.com/components/tabs/
   //https://material-ui.com/components/buttons/
@@ -63,15 +84,16 @@ function Header(props){
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <ToolBar disableGutters>
-            <img alt="company logo" src={logo} className={classes.logo}/>
+            <Button className={classes.logoContainer} component={Link} to="/" onClick={() => setValue(0)} disableRipple>
+              <img alt="company logo" src={logo} className={classes.logo}/>
+            </Button>
             <Tabs value={value} onChange={handleChange} className={classes.tabsContainer}
-            indicatorColor="secondary"
-            >
-              <Tab className={classes.tab} label="Home"/>
-              <Tab className={classes.tab} label="Services"/>
-              <Tab className={classes.tab} label="The Revolution"/>
-              <Tab className={classes.tab} label="About Us"/>
-              <Tab className={classes.tab} label="Contact Us"/>
+            indicatorColor="secondary">
+              <Tab className={classes.tab} component={Link} to="/home" label="Home"/>
+              <Tab className={classes.tab} component={Link} to="/services" label="Services"/>
+              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"/>
+              <Tab className={classes.tab} component={Link} to="/about" label="About Us"/>
+              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us"/>
             </Tabs>
             <Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
           </ToolBar>

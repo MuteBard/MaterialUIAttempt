41968a11984bc3beff937ff3b7cd77fe01c3274b
MuteBard <csevere@brandeis.edu>
2020-05-07 12:16:52 -0400

    optional removal of indicator

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -51,6 +51,10 @@ const useStyles = makeStyles(theme => ({
 function Header(props){
 
   const classes = useStyles()
+  const [value, setValue] = useState(0)
+  const handleChange = (e, value) => {
+    setValue(value)
+  }
 
   //https://material-ui.com/components/tabs/
   //https://material-ui.com/components/buttons/
@@ -60,7 +64,9 @@ function Header(props){
         <AppBar position="fixed" color="primary">
           <ToolBar disableGutters>
             <img alt="company logo" src={logo} className={classes.logo}/>
-            <Tabs className={classes.tabsContainer}>
+            <Tabs value={value} onChange={handleChange} className={classes.tabsContainer}
+            indicatorColor="secondary"
+            >
               <Tab className={classes.tab} label="Home"/>
               <Tab className={classes.tab} label="Services"/>
               <Tab className={classes.tab} label="The Revolution"/>
